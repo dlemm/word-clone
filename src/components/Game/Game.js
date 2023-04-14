200c16fab09bea8323c62b82b6b2d86a514803c3
Dennis Lemm <28383171+dlemm@users.noreply.github.com>
2023-04-14 14:11:33 +0200

    add toast and retry button

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -8,8 +8,6 @@ import Toast from '../Toast/Toast';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
-// To make debugging easier, we'll log the solution in the console.
-console.info({ answer });
 
 function Game() {
   const [guesses, setGuesses] = React.useState([]);
--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -7,14 +7,27 @@ function Toast({ variant, guesses, answer }) {
         <div className="happy banner">
           <p>
             <strong>Congratulations!</strong> Got it in
-            <strong> {guesses} {guesses > 1 ? "guesses" : "guess"}</strong>.
+            <strong>
+              {' '}
+              {guesses} {guesses > 1 ? 'guesses' : 'guess'}
+            </strong>
           </p>
+          <form>
+            <button type="submit" className="retry">
+              Play again
+            </button>
+          </form>
         </div>
       ) : (
         <div className="sad banner">
           <p>
             Sorry, the correct answer is <strong>{answer}</strong>.
           </p>
+          <form>
+            <button type="submit" className="retry">
+              Play again
+            </button>
+          </form>
         </div>
       )}
     </>
